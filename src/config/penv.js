6618c4f5f5c1afba6012fed95eaa2a6b64abe394
axtck <alexandertack@hotmail.be>
2021-09-29 21:38:52 +0200

    Add config

---
+++ src/config/penv.js
@@ -0,0 +1,7 @@
+require("dotenv").config();
+
+const penv = {
+    port: process.env.PORT
+};
+
+module.exports = penv;
--- src/server.js
+++ src/server.js
@@ -1,1 +1,10 @@
-// server file
+const express = require("express");
+const app = express();
+const mysql = require("mysql");
+const penv = require("./config/penv");
+
+console.log(`Environment variables: ${JSON.stringify(penv)}`);
+
+
+const port = penv.port || 5001;
+app.listen(port, () => console.log(`Listening on ${port}`));
